282866141fa126676b383f8c16ead44748d756de
EucTech <euccreative@gmail.com>
2024-01-13 12:26:13 +0100

    1-promise.js

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
